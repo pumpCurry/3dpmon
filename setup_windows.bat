cf812e9fa57c153ea8d20a41d59dec59047d240a
pumpCurry <pcb@hogera.com>
2025-06-24 12:58:30 +0900

    Revert "Add Windows installation docs and setup scripts"

--- setup_windows.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM Setup virtual environment and install dependencies
-python -m venv venv
-call .\venv\Scripts\activate
-python -m pip install --upgrade pip
-pip install -r requirements.txt
